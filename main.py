2edcb10517c1abe4a9456a4b992e23bc050e4ccc
soroush <sm@ubuntu>
2021-07-17 05:01:40 -0700

    add error check

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from lxml import html
 import requests
+from datetime import datetime
+import info
 
+def send_sms(message):
+	print('Sending ...')
+
+	url = 'https://api.kavenegar.com/v1/%s/sms/send.json' % info.key
+	Data = {'receptor' : info.receptor,'message':message}
+
+	r = requests.post(url, data = Data)
+	if r.ok:
+		print("send")	
+
+"""
 dic = {
 "۰":"0",
 "۱":"1",
@@ -13,6 +26,7 @@ dic = {
 "۸":"8",
 "۹":"9"
 }
+
 numbers = [str(i) for i in range(0, 10)]
 cpital_word = [chr(i) for i in range(ord('a'), ord('z') + 1)]
 small_word = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
@@ -26,8 +40,7 @@ def del_word_fa(string):
 				new_word += char
 	return new_word
 
-
-def eng_to_fa(num):
+def conver_number(num):
 	new_num = ""
 	for i in num:
 		try:
@@ -35,8 +48,6 @@ def eng_to_fa(num):
 		except:
 			pass
 	return int(new_num)	
-
-
 
 phones = {'https://torob.com/p/b48f4c3d-1930-498f-a9d1-02880aca8565/%DA%AF%D9%88%D8%B4%DB%8C-%D8%B4%DB%8C%D8%A7%DB%8C%D9%88%D9%85%DB%8C-redmi-note-9t-5g-%D8%AD%D8%A7%D9%81%D8%B8%D9%87-128-%D8%B1%D9%85-4-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA/':4300000
 
@@ -47,21 +58,19 @@ phones = {'https://torob.com/p/b48f4c3d-1930-498f-a9d1-02880aca8565/%DA%AF%D9%88
 ,'https://torob.com/p/1cc174ed-11cd-4d9a-b205-d9dc3f2afe51/%DA%AF%D9%88%D8%B4%DB%8C-%D8%B4%DB%8C%D8%A7%DB%8C%D9%88%D9%85%DB%8C-poco-m3-pro-5g-%D8%AD%D8%A7%D9%81%D8%B8%D9%87-128-%D8%B1%D9%85-6-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA/':4300000
 
 ,'https://torob.com/p/d0149344-4a4e-4e73-910b-da19a4211e44/%DA%AF%D9%88%D8%B4%DB%8C-%D8%B4%DB%8C%D8%A7%DB%8C%D9%88%D9%85%DB%8C-redmi-note-10-%D8%AD%D8%A7%D9%81%D8%B8%D9%87-128-%D8%B1%D9%85-4-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA/':4300000
-
-
-
-
-,'https://torob.com/p/bd14511b-1be6-4ef7-876d-66ec2d84b5d2/%DA%AF%D9%88%D8%B4%DB%8C-%D8%B4%DB%8C%D8%A7%DB%8C%D9%88%D9%85%DB%8C-poco-x3-%D8%AD%D8%A7%D9%81%D8%B8%D9%87-64-%D8%B1%D9%85-6-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA/':9
 }
 
-
 for url, buy in phones.items():
-	page = requests.get(url)
-	content = html.fromstring(page.content)
-	data = content.xpath('//*[@id="0"]/text()')
-	name = del_word_fa(data[1])
-	sell = eng_to_fa(data[3])
-	if sell <= buy:
-		print("i have buy phone " + name + " sell " + str(sell) + " buy " + str(buy))
-	else:
-		print("i have not buy phone " + name + " sell " + str(sell) + " buy " + str(buy))
+	try:
+		page = requests.get(url)
+		content = html.fromstring(page.content)
+		data = content.xpath('//*[@id="0"]/text()')
+		name = del_word_fa(data[1])
+		sell = conver_number(data[3])
+		if sell <= buy:
+			print(name + " sell " + str(sell) + " buy " + str(buy))
+		else:
+			print('!!! Not Buy Now ' + name + " sell: " + str(sell) + '  At  ' + 					datetime.now().strftime('%Y-%m-%d %H:%M:%S'))   
+	except:
+		print('Not Internet At ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+"""
